27c09a024bbe4c5551559fafe5633130dfeaae9a
shashankyc2002 <shashankyc010@gmail.com>
2025-08-12 20:32:03 +0530

    initial

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.getElementById("fetchTitle").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const title = tabs[0].title;
+    document.getElementById("title").textContent = title;
+  });
+});
